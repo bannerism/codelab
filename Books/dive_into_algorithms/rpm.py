7f5f2db7edfbd16246c4f50e821dd559473c159b
Jeffrey Banner <jebanner@gmail.com>
2023-02-08 11:14:26 -0700

    fix: removed dependencies and made faster

--- Books/dive_into_algorithms/rpm.py
+++ Books/dive_into_algorithms/rpm.py
@@ -1,23 +1,52 @@
+def halving(larger):
+    halving = []
+    while larger > 0:
+        halving.append(larger)
+        larger = larger // 2
+    return halving
 
-import math 
-import pandas as pd
+def double(smaller):
+   return smaller * 2
 
-def rpm(n1,n2):
-    halving = [n1]
+def doubling(smaller, left):
+    doubling = []
+    for i in range(len(left)):
+        doubling.append(smaller)
+        smaller = double(smaller)
 
-    while(min(halving) > 1):
-        halving.append(math.floor(min(halving)/2))
+    return doubling
 
-    doubling =[n2]
-    while(len(doubling)< len(halving)):
-        doubling.append(max(doubling)* 2) 
+def sum_odd_halving(halvings, doublings):
+    candidates = list(zip(halvings, doublings))
+    to_sum = []
+    for c in candidates:
+        if c[0] % 2:
+            to_sum.append(c[1])
 
-    half_double = pd.DataFrame(zip(halving,doubling))
-    half_double = half_double.loc[half_double[0]%2 == 1,:]
-    return sum(half_double.loc[:,1])
+    return to_sum
+
+def rpm(n1:int, n2:int)-> int:
+    """RPM, Russian Peasant Multiplication
+    
+    Multiply two numbers by halving the larger number and doubling the smaller
+
+    Args:
+        n1 (int): number to multiply
+        n2 (int): second number to multiply
+
+    Returns:
+        int: the product of n1 * n2
+    """
+    values = [n1, n2]
+    larger = max(values)
+    smaller = min(values)
+    left = halving(larger)
+    right = doubling(smaller, left)
+    return sum(sum_odd_halving(left, right))
 
 if __name__ == "__main__":
     n1 = 89
-    n2 = 18
-    print(rpm(n1,n2))
-    
+    n2 = 17
+    result = rpm(n1, n2)
+    assert result == n1 * n2
+    print(result)
