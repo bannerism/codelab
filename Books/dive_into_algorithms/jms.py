adf0b2a4f4a524f3813e2edf71f1f83b9536902e
Jeffrey Banner <jebanner@gmail.com>
2023-02-09 13:18:16 -0700

    fix: DRY improvements

--- Books/dive_into_algorithms/jms.py
+++ Books/dive_into_algorithms/jms.py
@@ -1,48 +1,41 @@
 ## Japanese Magic Squares
 
-def row_sums(given_matrix):
-    sums = []
-    rowsums = [sum(given_matrix[i]) for i in range(0, len(given_matrix))]
-    sums.append(rowsums)
-    return sums
-    
-def col_sums(given_matrix):
-    sums = []
-    colsums = [sum([row[i] for row in given_matrix]) for i in range(0, len(given_matrix))]
-    sums.append(colsums)
-    return sums
+def parse_magic_square(given_matrix):
+    row = []
+    columns = []
+    major_diagonal = []
+    minor_diagonal = []
 
-def major_sums(given_matrix):
-    sums = []
-    major_diagonal = sum([given_matrix[i][i] for i in range(0, len(given_matrix))])
-    sums.append([major_diagonal])
-    return sums
+    for i, x in enumerate(given_matrix):
+        row.append(sum(x))
 
-def minor_sums(given_matrix):
-    sums = []
-    minor_diagonal = sum([given_matrix[i][len(given_matrix) - 1 - i] for i in range(0, len(given_matrix))])
-    sums.append([minor_diagonal])
-    return sums
+        columns.append(sum([row[i] for row in given_matrix]))
 
-def flatten(sums):
-    flattened = [j for i in sums for j in i]
-    return flattened
+        major_diagonal.append(given_matrix[i][i])
+
+        minor_diagonal.append(given_matrix[i][len(given_matrix) - 1 - i])
+
+    return row, columns, [sum(major_diagonal)], [sum(minor_diagonal)]
+
+def verify_equality(sums_of_square):
+    sums = []
+    for i in sums_of_square:
+       for j in i:
+           sums.append(j)
+
+    return len(list(set(sums))) == 1
 
 def verify_square(given_matrix):
-    sums = []
-    sums.append(row_sums(given_matrix))
-    sums.append(col_sums(given_matrix))
-    sums.append(major_sums(given_matrix))
-    sums.append(minor_sums(given_matrix))    
-    flattened = flatten(sums)[0]
-    return (len(list(set(flattened))) == 1)
+    sums = parse_magic_square(given_matrix)
+    return verify_equality(sums)
 
 if __name__ == "__main__":
     import numpy as np
     size_by = (3,3)
-    square = np.arange(1, 10).reshape(size_by)
-    luoshu = np.array([4,9,2,3,5,7,8,1,6]).reshape(3,3)
+    square = np.arange(1, 10).reshape(size_by) #False
+    luoshu = np.array([4,9,2,3,5,7,8,1,6]).reshape(3,3) #True
 
     print(verify_square(luoshu))
+
 
 
